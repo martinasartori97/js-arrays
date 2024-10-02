d162ef7bfa0b420310c905c65c09a9e3fd2dd369
martinasartori97 <martinasartori9@gmail.com>
2024-10-02 15:31:30 +0200

    edit js

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -29,7 +29,9 @@ console.log(teachers);
 
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers.shift();
+console.log(teachers);
+
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
 
